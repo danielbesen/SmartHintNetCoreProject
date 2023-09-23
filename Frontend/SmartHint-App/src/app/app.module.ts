6b18d5925bf5187e219c5c164f1973fafe1affaa
danielbesen <nielbesen@gmail.com>
2023-09-23 01:25:53 -0300

    Navbar component added

--- Frontend/SmartHint-App/src/app/app.module.ts
+++ Frontend/SmartHint-App/src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomerService } from './services/customer.service';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, NavbarComponent],
+  imports: [BrowserModule, AppRoutingModule, CollapseModule],
   providers: [CustomerService],
   bootstrap: [AppComponent],
 })
