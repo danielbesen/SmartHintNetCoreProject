f451b38e87a17f9e02f1cd1c5e51e4da97836ef4
danielbesen <nielbesen@gmail.com>
2023-09-22 22:30:36 -0300

    Added Dtos folder and created CustomerDto and validations

--- Backend/src/SmartHint.Application/Dtos/CustomerDto.cs
+++ Backend/src/SmartHint.Application/Dtos/CustomerDto.cs
@@ -1,18 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SmartHint.Application.Dtos
 {
     public class CustomerDto
     {
         public int Id { get; set; }
+        [Required, MaxLength(150)]
         public string Name { get; set; }
+        [Required]
         public string Type { get; set; }
+        [Required, MaxLength(150), EmailAddress]
         public string Email { get; set; }
+        [Required, MaxLength(11)]
         public string Phone { get; set; }
         public DateTime? Date { get; set; }
+        [Required]
         public string IdentityDocument { get; set; }
         public string StateRegistration { get; set; }
         public string Gender { get; set; }
         public DateTime? DateOfBirth { get; set; }
         public bool IsBlocked { get; set; }
+        [Required, MinLength(8), MaxLength(15)]
         public string Password { get; set; }
     }
 }
