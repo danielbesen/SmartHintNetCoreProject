031d09215e8d132d492d362f3de9f69e3f828a45
danielbesen <nielbesen@gmail.com>
2023-09-22 23:16:16 -0300

    Added dependency injection of interfaces at program and cors

--- Backend/src/SmartHint.API/Program.cs
+++ Backend/src/SmartHint.API/Program.cs
@@ -1,29 +1,37 @@
 using Microsoft.EntityFrameworkCore;
+using SmartHint.Application.Interfaces;
+using SmartHint.Application.Services;
 using SmartHint.Persistance.Context;
+using SmartHint.Persistance.Interfaces;
+using SmartHint.Persistance.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson(x => x.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddScoped<ICustomerService, CustomerService>();
+builder.Services.AddScoped<IGeneralPersist, GeneralPersist>();
+builder.Services.AddScoped<ICustomerPersist, CustomerPersist>();
+
+builder.Services.AddCors();
 
 var connectionString = builder.Configuration.GetConnectionString("DbConnection");
 builder.Services.AddDbContext<SmartHintContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(access => access.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
 app.UseHttpsRedirection();
 
