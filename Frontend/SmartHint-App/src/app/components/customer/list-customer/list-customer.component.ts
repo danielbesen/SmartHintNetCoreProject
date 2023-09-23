5b8e149df1fbfa74835f78c71f6307c31c9a251a
danielbesen <nielbesen@gmail.com>
2023-09-23 03:54:42 -0300

    Checkbox grid functionality created

--- Frontend/SmartHint-App/src/app/components/customer/list-customer/list-customer.component.ts
+++ Frontend/SmartHint-App/src/app/components/customer/list-customer/list-customer.component.ts
@@ -35,4 +35,44 @@ export class ListCustomerComponent implements OnInit {
   detailCustomer(id: number): void {
     this.router.navigate([`customer/detail/${id}`]);
   }
+
+  selectedRows: number[] = [];
+
+  selectAll(event: Event) {
+    const checkbox = event.target as HTMLInputElement;
+    if (checkbox.checked) {
+      this.selectedRows = Array.from(
+        { length: this.customers.length },
+        (_, i) => i
+      );
+    } else {
+      // Deselect all rows
+      this.selectedRows = [];
+    }
+  }
+
+  toggleRow(index: number) {
+    if (this.selectedRows.includes(index)) {
+      // Row is selected, deselect it
+      this.selectedRows = this.selectedRows.filter(
+        (rowIndex) => rowIndex !== index
+      );
+    } else {
+      this.selectedRows.push(index);
+    }
+  }
+
+  isRowSelected(index: number): boolean {
+    return this.selectedRows.includes(index);
+  }
+
+  updateSelectAll() {
+    const allSelected = this.selectedRows.length === this.customers.length;
+    const selectAllCheckbox = document.querySelector(
+      '#selectAll'
+    ) as HTMLInputElement;
+    if (selectAllCheckbox) {
+      selectAllCheckbox.checked = allSelected;
+    }
+  }
 }
